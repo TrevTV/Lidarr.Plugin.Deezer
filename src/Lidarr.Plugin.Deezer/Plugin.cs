4df6fef0d55d70ea31e46c0b2a95861ec905acbc
Trevor <me@trev.app>
2024-08-17 19:56:55 -0400

    Rename everything

---
+++ src/Lidarr.Plugin.Deezer/Plugin.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NzbDrone.Core.Plugins
+{
+    public class DeezerPlugin : Plugin
+    {
+        public override string Name => "Deezer";
+        public override string Owner => "TrevTV;ta264";
+        public override string GithubUrl => "https://github.com/TrevTV/Lidarr.Plugin.Deezer";
+    }
+}
