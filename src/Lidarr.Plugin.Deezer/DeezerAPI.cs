3dace142122831bceddf4f8784d6792bfe295bf2
Trevor <me@trev.app>
2024-08-17 22:21:25 -0400

    Use DeezNET inside DeezerAPI
    Yes, I'm wrapping a wrapper. It makes it a tad cleaner and easier

--- src/Lidarr.Plugin.Deezer/DeezerAPI.cs
+++ src/Lidarr.Plugin.Deezer/DeezerAPI.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Net;
 using System.Text;
 using System.Linq;
+using DeezNET;
 
 namespace NzbDrone.Plugin.Deezer
 {
@@ -14,86 +15,24 @@ namespace NzbDrone.Plugin.Deezer
         internal DeezerAPI(string arl)
         {
             Instance = this;
-            _apiToken = "null"; // this doesn't necessarily need to be null; used for deezer.getUserData
-            _arl = arl;
+            _client = new();
             UpdateArl(arl);
         }
 
-        internal string _arl;
-        internal string _apiToken;
-        private JToken _activeUserData;
+        public DeezerClient Client => _client;
+
+        private DeezerClient _client;
+        private string _apiToken => _client.GWApi.ActiveUserData["checkForm"]!.ToString();
 
         internal void UpdateArl(string arl)
         {
-            _arl = arl;
             if (string.IsNullOrEmpty(arl))
             {
                 // TODO: this would be where to grab one from firehawk
                 return;
             }
 
-            var userData = GetUserData();
-            _activeUserData = userData;
-            _apiToken = userData["checkForm"]!.ToString();
-        }
-
-        private JToken GetUserData() => Call("deezer.getUserData", needsArl: true);
-
-        private JToken Call(string method, JObject args = null, Dictionary<string, string> parameters = null, bool needsArl = false)
-        {
-            parameters ??= [];
-            parameters["api_version"] = "1.0";
-            parameters["api_token"] = _apiToken;
-            parameters["input"] = "3";
-            parameters["method"] = method;
-
-            var body = args?.ToString() ?? "";
-            StringContent stringContent = new(body);
-
-            StringBuilder stringBuilder = new("https://www.deezer.com/ajax/gw-light.php");
-            for (var i = 0; i < parameters.Count; i++)
-            {
-                var start = i == 0 ? "?" : "&";
-                var key = WebUtility.UrlEncode(parameters.ElementAt(i).Key);
-                var value = WebUtility.UrlEncode(parameters.ElementAt(i).Value);
-                stringBuilder.Append(start + key + "=" + value);
-            }
-
-            var url = stringBuilder.ToString();
-
-            using HttpRequestMessage request = new(HttpMethod.Post, url)
-            {
-                Content = stringContent
-            };
-
-            if (needsArl)
-                request.Headers.Add("Cookie", "arl=" + _arl);
-
-            using HttpClient client = new();
-            client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.5");
-            var response = client.Send(request);
-
-            var readTask = response.Content.ReadAsStringAsync();
-            readTask.Wait();
-            var resp = readTask.Result;
-            var json = JObject.Parse(resp);
-
-            var error = json["error"];
-            if (error != null && error.Any())
-            {
-                if (error["VALID_TOKEN_REQUIRED"] != null || error["GATEWAY_ERROR"] != null)
-                {
-                    UpdateArl(_arl);
-                    return Call(method, args, parameters, needsArl);
-                }
-
-                if (error["DATA_ERROR"] != null)
-                    throw new System.Exception("The given ID is not valid. It either does not exist or is not for the requested content type.");
-
-                throw new System.Exception(error.ToString());
-            }
-
-            return json["results"]!;
+            _client.SetARL(arl).Wait();
         }
 
         public string GetGWUrl(string method, Dictionary<string, string> parameters = null)
